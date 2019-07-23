ff5b5c814b69c9679ac6c74c90ddb6cb6e4f49d0
Perluceo <FullStackDevSMU@gmail.com>
2019-07-22 21:00:51 -0500

    seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bourbon bbq burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("steak burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("beer cheese bacon burger", 1);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("pizza burger", 1);
