9e39f610f12115ab222362b19c0579a1c771610c
Perluceo <FullStackDevSMU@gmail.com>
2019-08-07 12:23:41 -0500

    controller.js file cleanup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,42 +6,36 @@ var burger = require("../models/burger.js");
 
 router.get('/', function (req, res) {
   res.redirect('/index');
-})
+});
 
 router.get("/index", function (req, res) {
   burger.selectAll(function (data) {
     var dbObject = { burgers: data };
     console.log(dbObject);
-    
+
     res.render("index", dbObject);
   });
 });
 
 
-router.post("/api/burger", function (req, res) {
-  burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
-    res.json({ id: result.insertID });
+router.post("/burgers/insertOne", function (req, res) {
+  burger.insertOne(["burger_name", "devoured"], [req.body.name, false], function () {
+    res.redirect('/index');
   });
 
 });
 
-router.put("/api/burger/:id", function (req, res) {
-  var condition = "id = " + req.params.id;
+router.put("/burgers/updateOne:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
 
-  console.log("condition", condition);
+    
 
-  burger.updateOne({
-    devoured: req.body.devoured
-  }, condition, function (result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+    burger.updateOne({ devoured: req.body.devoured }, condition, function () {
+      res.redirect('/index');
+    });
   });
-});
 
 
 
-module.exports = router;
+  module.exports = router;
