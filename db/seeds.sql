19c80fa9cc78bd5c01d7edddbf7756eb0f54d530
Perluceo <FullStackDevSMU@gmail.com>
2019-08-07 16:35:26 -0500

    cleaned seeds and index.hndbrs

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured, dt)
-VALUES ("bourbon bbq burger", 0, CURRENT_TIMESTAMP),
-("steak burger", 0),
-("beer cheese bacon burger", 1),
-("pizza burger", 1);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bourbon bbq burger", false),
+("steak burger", false),
+("beer cheese bacon burger", false),
+("pizza burger", false);
